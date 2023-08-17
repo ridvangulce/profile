ab1194f2ba55aa37663b4cf656e268538639a2b6
ridvangulce <ridvangulce@gmail.com>
2023-08-18 00:39:16 +0300

    +added linkstacks

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -15,44 +15,6 @@ const Portfolio = () => {
       clearTimeout(timer)
     }
   })
-
-
-
-
-  // useEffect(() => {
-  //   getPortfolio()
-  // }, [])
-
-  // const getPortfolio = async () => {
-  //   const querySnapshot = await getDocs(collection(db, 'portfolio'))
-  //   setPortfolio(querySnapshot.docs.map((doc) => doc.data()))
-  // }
-
-  // const renderPortfolio = (portfolio) => {
-  //   return (
-  //     <div className="images-container">
-  //       {portfolio.map((port, idx) => {
-  //         return (
-  //           <div className="image-box" key={idx}>
-  //             <img
-  //               src={port.image}
-  //               className="portfolio-image"
-  //               alt="portfolio"
-  //             />
-  //             <div className="content">
-  //               <p className="title">{port.name}</p>
-  //               <h4 className="description">{port.description}</h4>
-  //               <button className="btn" onClick={() => window.open(port.url)}>
-  //                 View
-  //               </button>
-  //             </div>
-  //           </div>
-  //         )
-  //       })}
-  //     </div>
-  //   )
-  // }
-
   return (
     <>
       <div className="container portfolio-page">
