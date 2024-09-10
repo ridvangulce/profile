699f02c0a1ff39b942bd346db25aa49e7dcff637
ridvangulce <ridvangulce@gmail.com>
2024-09-10 20:45:42 +0300

    +edited resume

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -7,7 +7,7 @@ const Footer = ({}) => {
   return (
     <>
       <div className="mt-5 laptop:mt-40 p-2 laptop:p-0">
-        <div>
+        {/* <div>
           <h1 className="text-2xl text-bold">Contact.</h1>
           <div className="mt-10">
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
@@ -21,7 +21,7 @@ const Footer = ({}) => {
               <Socials />
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
         Made With ❤ by{" "}
