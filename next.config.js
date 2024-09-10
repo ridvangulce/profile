5af51715207a16641c7964f59322b9ee14f4d8aa
ridvangulce <ridvangulce@gmail.com>
2024-09-10 21:00:25 +0300

    +added new config on next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'query',
+            key: 'endsWithSlash',
+          },
+        ],
+        destination: '/:path*/',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'query',
+            key: 'noSlash',
+          },
+        ],
+        destination: '/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
